fd0f8d6822cf2e0d5e1e7e4683d1d65a88596b77
tinykofi <kofiasare163@gmail.com>
2025-06-15 19:11:36 +0000

    Add Database queries

---
+++ Lab1.sql
@@ -0,0 +1,11 @@
+--Create DB
+CREATE DATABASE Lab1
+
+--Create Table
+CREATE TABLE users
+(
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL,
+    email TEXT UNIQUE NOT NULL,
+    password TEXT NOT NULL
+);
---
+++ db_backup.sql
@@ -0,0 +1,6 @@
+-- db_backup.sql
+
+INSERT INTO users
+    (name, email, password)
+VALUES
+    ('admin', 'kofi@admin.com', '$2b$10$ocYBHQaj98UXr4nvO4D6FuG2dkE8PUkiy1.yO81MkHYM/qCn0Yk5S'); -- password: "12345678"
